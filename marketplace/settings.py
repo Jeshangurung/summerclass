3127e90816652b9de1437e867c5c78a8da14ed61
Jeshangurung <jeshangurung012@gmail.com>
2025-08-08 09:17:13 +0545

    Adding custom domain

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -29,8 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'summerclass-b7ck.onrender.com', #render URL
-    '12.0.0.1', # FOR LOCAL DEVELOPMENT
+    '127.0.0.1', # FOR LOCAL DEVELOPMENT
     'localhost', # OPTIONAL
+    'jeshangurung.com.np' 
 ]
 
 
@@ -87,16 +88,16 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+#DATABASES = {
+   # 'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
 
 DATABASES ={
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+       default=os.environ.get('DATABASE_URL')
     )
 }
 
